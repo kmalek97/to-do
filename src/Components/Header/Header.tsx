f96f90ae82fcf88f06860aecd742d2d6dd34d654
Konrad Malek <kmalek94@gmail.com>
2021-02-07 14:55:02 +0100

    start using css-modules and finish Header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import './_Header.scss'
+import HeaderCSS from './Header.module.scss';
 
 export default function Header() {
   return (
-    <div className="header">
+    <div className={HeaderCSS.header}>
       <h1>Lista rzeczy do zrobienia</h1>
     </div>
   )
