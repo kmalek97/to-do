52a613860d14f63ae0847a73a66a00d7e3ecc6b6
Konrad Malek <kmalek94@gmail.com>
2021-02-08 02:31:52 +0100

    add toggle button

--- src/Components/ListElement/ListElement.tsx
+++ src/Components/ListElement/ListElement.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ListElementCSS from './ListElement.module.scss';
 
@@ -7,7 +7,19 @@ type CardProps = {
 }
 
 export default function ListElement ({title}: CardProps) {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleItemChange = (e: { preventDefault: () => void; }) => {
+    setIsActive(!isActive);
+  }
+
+  const doneItem = ListElementCSS.p_off;
+
   return (
-    <li className={ListElementCSS.list_element}><p>{title}</p></li>
+    <li className={ListElementCSS.list_element}>
+      <p className={isActive ? doneItem : ''}>{title}</p>
+      <button onClick={handleItemChange}>{ isActive ? 'Cofnij' : 'Zrobione!' }</button>
+    </li>
   )
 }
